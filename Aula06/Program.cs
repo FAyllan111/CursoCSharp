c08a970c4c9b30167e565ef8032e0b03790e840b
Ayllan Alves <franciscoayllan@gmail.com>
2024-11-13 17:36:13 -0300

    chore: 1/2 aula 6 - internet caiu

--- Aula06/Program.cs
+++ Aula06/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//FORMATACAO NA SAIDA DO CONSOLE
+
+class Aula06{
+
+    static void Main(){
+        int n1, n2, n3;
+
+        n1 = 54;
+        n2 = 009;
+        n3 = 001992;
+
+        Console.Write(n1 + "," + n2 + "," + n3);// concatenando com +
+        Console.Write("n1 = {0}, n2= {1} , n3={2}",n1,n2,n3);// usando indices caracteres de escapes \n \t , caracteres de formatacao
+
+
+        double valorCompras = 69.8;
+        double valorVenda;
+        double lucro = 0.15;
+        string finalizar = "";
+
+
+    }
+}
