5c28f57b74aa7eb212ffb103dc15985fb69aaaa5
Ayllan Alves <franciscoayllan@gmail.com>
2024-11-13 15:32:53 -0300

    Initial Commit : inicio das aulas

---
+++ Aula01/aula01.cs
@@ -0,0 +1,6 @@
+using System; // comandos basicos do sistema de entrada e saida vem dessa biblioteca
+class Principal{
+    public static void Main(){ // classe principal 
+        Console.WriteLine("Olá mundo!!");// escreve o que vem 
+    }
+}
