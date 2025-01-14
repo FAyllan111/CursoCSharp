4d0566f56d302895c222e9a94c9ff3ab2f87d155
Ayllan Alves <franciscoayllan@gmail.com>
2025-01-14 08:44:42 -0300

    chore : atualização

--- Aula17/obj/Debug/net8.0/Aula17.AssemblyInfo.cs
+++ Aula17/obj/Debug/net8.0/Aula17.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula17")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5876efd7c84d3ab73d15a2afdd1b1f1ac264cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a2deea4271efe994f31680ff1effc7f812f676")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula17")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula17")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
